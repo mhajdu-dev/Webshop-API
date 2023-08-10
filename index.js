ddd0fdb3e3b763051127feaca79dcc39403ed477
Marci <hajdumarcel@gmail.com>
2023-08-10 09:41:59 +0200

    express async errors package installed

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 require('dotenv').config()
+require('express-async-errors')
 
 const express = require('express')
 const app = express()
 
 const connectDB = require('./db/connect')
+
+const notFoundMiddleware = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
 
 app.use(express.json())
 
@@ -11,6 +15,9 @@ app.use(express.json())
 app.get('/', (req, res) => {
     res.send('Hello e-commerce!')
 })
+
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
 
 
 const port = process.env.PORT || 3000;
