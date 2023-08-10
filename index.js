b516ddd1133092839fb842d32af85b04241899ae
Marci <hajdumarcel@gmail.com>
2023-08-10 09:36:09 +0200

    status code package installed&middlewares imported

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
 
-const port = 3000;
+const connectDB = require('./db/connect')
+
+app.use(express.json())
+
 
 app.get('/', (req, res) => {
     res.send('Hello e-commerce!')
 })
 
-app.listen(port, () => {
-    console.log(`Web app listening on port ${port}`)
-})
+
+const port = process.env.PORT || 3000;
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, () => {
+            console.log(`Web app listening on port ${port}`)
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
+
+
+
