4b7490f755617e4560c3553bfc0c04b7f2ef988a
Marci <hajdumarcel@gmail.com>
2023-08-13 12:35:18 +0200

    logout functionality implemented

--- controllers/authController.js
+++ controllers/authController.js
@@ -43,7 +43,11 @@ const login = async (req, res) => {
 }
 
 const logout = async (req, res) => {
-    res.send('logout user')
+    res.cookie('token', 'logout', {
+        httpOnly: true,
+        expires: new Date(Date.now())
+    })
+    res.status(StatusCodes.OK).json({ msg: 'logout successful' })
 }
 
 module.exports = {
