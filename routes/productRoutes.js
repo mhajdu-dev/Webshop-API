d5c7a3e0a8117e359aaef75770a9a65dc6a83f14
Marci <hajdumarcel@gmail.com>
2023-08-17 12:22:39 +0200

    basic productController done

---
+++ routes/productRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const router = express.Router()
+const { authenticateUser, authorizePermissions } = require('../middleware/authentication')
+
+const {
+    createProduct, getAllProducts, getSingleProduct, updateProduct, deleteProduct, uploadImage
+} = require('../controllers/productController')
+
