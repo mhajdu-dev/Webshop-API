7727b097ce9f2a88fbd74a431811aeba1858aa01
Marci <hajdumarcel@gmail.com>
2023-08-17 12:30:09 +0200

    productRoutes implemented

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -6,3 +6,10 @@ const {
     createProduct, getAllProducts, getSingleProduct, updateProduct, deleteProduct, uploadImage
 } = require('../controllers/productController')
 
+router.route('/').post([authenticateUser, authorizePermissions('admin')], createProduct).get(getAllProducts)
+
+router.route('/uploadImage').post([authenticateUser, authorizePermissions('admin')], uploadImage)
+
+router.route('/:id').get(getSingleProduct).patch([authenticateUser, authorizePermissions('admin')], updateProduct).delete([authenticateUser, authorizePermissions('admin')], deleteProduct)
+
+module.exports = router
