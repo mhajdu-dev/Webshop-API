017488fd76222d5012c756c759f80905245ed6ab
Marci <hajdumarcel@gmail.com>
2023-08-21 12:39:16 +0200

    deleteReview completed

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -46,7 +46,23 @@ const getSingleReview = async (req, res) => {
 }
 
 const updateReview = async (req, res) => {
-    res.send('update a review')
+    const { id: reviewId } = req.params
+    const { rating, title, comment } = req.body
+
+    const review = await Review.findOne({ _id: reviewId })
+
+    if (!review) {
+        throw new CustomError.NotFoundError(`No review with this id: ${reviewId}`)
+    }
+
+    checkPermissions(req.user, review.user)
+
+    review.rating = rating
+    review.title = title
+    review.comment = comment
+
+    await review.save()
+    res.status(StatusCodes.OK).json({ review })
 }
 
 const deleteReview = async (req, res) => {
@@ -60,7 +76,6 @@ const deleteReview = async (req, res) => {
 
     checkPermissions(req.user, review.user)
     await Review.deleteOne({ _id: reviewId })
-
 
     res.status(StatusCodes.OK).json({ msg: 'Success! Review removed' })
 }
